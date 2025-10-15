996b8ad9122db852f92855ae40950c0512ec7fd3
SofiaKubo <skuboreva@gmail.com>
2025-10-15 18:09:12 +0200

    feat (trainer-4-7): add function to append sum row to 2D array of numbers

---
+++ sprint_5/04-working-with-data-basics/trainer-4-7.js
@@ -0,0 +1,29 @@
+function getExcelData() {
+  const data = [
+    [12, 23, 34, 45],
+    [56, 67, 78, 89],
+    [90, 101, 112, 123],
+    [134, 145, 156, 167],
+    [178, 189, 190, 200],
+  ];
+
+  return data;
+}
+
+function addSumRow(data) {
+  let sum = [];
+  data.forEach((row) => {
+    row.forEach((col, index) => {
+      if (sum.length <= index) {
+        sum.push(0);
+      }
+      sum[index] = sum[index] + col;
+    });
+  });
+  data.push(sum);
+}
+
+let data = getExcelData();
+addSumRow(data);
+
+console.log(data);
