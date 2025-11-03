654fc4d4b62377b968c8c77aae8412c3c810a7d5
SofiaKubo <skuboreva@gmail.com>
2025-11-03 11:44:21 +0100

    feat: add total price calculation from list items

--- sprint_6/03-working-with-element-content/scripts/script.js
+++ sprint_6/03-working-with-element-content/scripts/script.js
@@ -4,3 +4,20 @@ const title = header.textContent.trim();
 
 console.log(title);
 header.textContent = title;
+
+const nodes = document.querySelectorAll('.list li');
+
+const totalPriceElement = document.querySelector('.totalPrice');
+
+let totalPrice = 0;
+
+nodes.forEach((element) => {
+  const [name, priceText] = element.textContent.split(':');
+
+  if (priceText) {
+    const price = Number(priceText.replace('руб.', '').trim());
+    totalPrice += price;
+  }
+});
+
+totalPriceElement.textContent = `Итого: ${totalPrice} руб.`;
