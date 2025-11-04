8a9d8df49720727f0c91f270e77ea8432351b216
SofiaKubo <skuboreva@gmail.com>
2025-11-04 11:40:50 +0100

    feat: enhance user card with field creation and document fragment optimization

--- sprint_6/05-creating-and-adding-elements/scripts/script.js
+++ sprint_6/05-creating-and-adding-elements/scripts/script.js
@@ -1,3 +1,4 @@
+document.createNode = document.createElement;
 const user = {
   name: 'Хабаров Валентин',
   age: 35,
@@ -5,18 +6,34 @@ const user = {
 };
 
 function createTitle(name) {
-  const title = document.createElement('h2');
-  title.classList.add('title');
-  title.textContent = name;
-  return title;
+  const h2 = document.createElement('h2');
+  h2.classList.add('title');
+  h2.textContent = name;
+
+  return h2;
+}
+
+function createField(key, value) {
+  const p = document.createElement('p');
+  const span = document.createElement('span');
+
+  span.classList.add('field');
+  span.textContent = `${key}: `;
+  p.append(span, String(value));
+
+  return p;
 }
 
 function createCard(user) {
   const card = document.createElement('article');
   card.classList.add('card');
 
-  const title = createTitle(user.name);
-  card.append(title);
+  card.append(createTitle(user.name));
+
+  const frag = document.createDocumentFragment();
+  frag.append(createField('Возраст', user.age), createField('Email', user.email));
+
+  card.append(frag);
   return card;
 }
 
