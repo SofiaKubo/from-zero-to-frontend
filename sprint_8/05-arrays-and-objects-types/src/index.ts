32369ee9f2f43ad5871ece321d3bd8dbc79d7f81
SofiaKubo <skuboreva@gmail.com>
2025-12-14 13:03:53 +0100

    feat(ts): add objects typing task

--- sprint_8/05-arrays-and-objects-types/src/index.ts
+++ sprint_8/05-arrays-and-objects-types/src/index.ts
@@ -2,3 +2,10 @@ const array1: string[] = ["a", "b", "c"];
 const array2: number[] = [1, 2, 4];
 const array3: boolean[] = [true, false];
 const array4: (string|number|boolean)[] = ["a", 2, true];
+
+
+const obj: {firstValue: string, secondValue: number, thirdValue: boolean} = {
+  firstValue: "myString",
+  secondValue: 123,
+  thirdValue: false,
+};
