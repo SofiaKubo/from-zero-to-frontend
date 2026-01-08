6432bbb43995d38ca0d0ffe652bf2a43b978463c
SofiaKubo <skuboreva@gmail.com>
2026-01-08 13:56:37 +0100

    feat: override message getter in CalculationError

--- sprint_8/09-inheritance-custom-errors/src/index.ts
+++ sprint_8/09-inheritance-custom-errors/src/index.ts
@@ -14,6 +14,17 @@ class CalculationError extends BaseError {
   constructor(code: string) {
     super(code);
 }
+
+  get message() {
+    switch (this.code) {
+      case 'DIVISION_BY_ZERO':
+        return 'Ошибка деления на ноль';
+      case 'INVALID_RANGE':
+        return 'Число должно быть в пределах от 5 до 10';
+      default:
+        return super.message;
+    }
+  }
 }
 
 const rangeError = new CalculationError('INVALID_RANGE');
