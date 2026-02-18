4b53380a4774d869a69dd7e020d961cfe4cefbf6
SofiaKubo <skuboreva@gmail.com>
2026-02-18 15:17:16 +0100

    update task use ref dom manipulation

--- sprint_10/11-use-ref-dom-manipulation/src/app/index.tsx
+++ sprint_10/11-use-ref-dom-manipulation/src/app/index.tsx
@@ -1,20 +1,16 @@
 import './styles.css';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 export const App = () => {
-  const headerRef = useRef<HTMLHeadingElement | null>(null);
+  const [isGold, setIsGold] = useState(false);
 
   const handleChangeColor = () => {
-    if (headerRef.current) {
-      headerRef.current.style.backgroundColor = 'gold';
-    }
+    setIsGold((prev) => !prev);
   };
 
   return (
     <div className="page">
-      <h1 className="header" ref={headerRef}>
-        Я меняю свой цвет
-      </h1>
+      <h1 className={`header${isGold ? ' header--gold' : ''}`}>Я меняю свой цвет</h1>
       <div className="card">
         <button onClick={handleChangeColor}>Изменить цвет</button>
       </div>
