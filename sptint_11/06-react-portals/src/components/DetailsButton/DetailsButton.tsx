6da5734a78e719e0f027df28ad2d82178ffc1d1b
SofiaKubo <skuboreva@gmail.com>
2026-03-09 12:40:28 +0100

    refactor: improve modal and details button structure

--- sptint_11/06-react-portals/src/components/DetailsButton/DetailsButton.tsx
+++ sptint_11/06-react-portals/src/components/DetailsButton/DetailsButton.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ReactDOM from 'react-dom';
 import { Modal } from '../Modal/Modal';
 
 export interface DetailsButtonProps {
@@ -8,10 +9,14 @@ export interface DetailsButtonProps {
 
 export const DetailsButton = ({ text, portalId }: DetailsButtonProps) => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const container = document.getElementById(portalId);
+
   return (
     <>
       <button onClick={() => setShowModal(true)}>Подробнее</button>
-      {showModal && <Modal onClose={() => setShowModal(false)} text={text} portalId={portalId} />}
+      {showModal &&
+        container &&
+        ReactDOM.createPortal(<Modal onClose={() => setShowModal(false)} text={text} />, container)}
     </>
   );
 };
--- sptint_11/06-react-portals/src/components/Modal/Modal.tsx
+++ sptint_11/06-react-portals/src/components/Modal/Modal.tsx
@@ -1,20 +1,15 @@
 import './styles.css';
-import ReactDOM from 'react-dom';
 
 interface ModalProps {
   onClose: () => void;
   text: string;
-  portalId: string;
 }
 
-export function Modal({ text, onClose, portalId }: ModalProps) {
-  const container = document.getElementById(portalId);
-
-  return ReactDOM.createPortal(
+export function Modal({ text, onClose }: ModalProps) {
+  return (
     <div className="modal">
       <div>{text}</div>
       <button onClick={onClose}>Закрыть</button>
-    </div>,
-    container!
+    </div>
   );
 }
