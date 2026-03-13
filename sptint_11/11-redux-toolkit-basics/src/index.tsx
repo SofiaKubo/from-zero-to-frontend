0d3c270b6e0e73b99f07318ccdb1ab9b3d8c3f95
SofiaKubo <skuboreva@gmail.com>
2026-03-13 12:23:26 +0100

    style: fix store import formatting

--- sptint_11/11-redux-toolkit-basics/src/index.tsx
+++ sptint_11/11-redux-toolkit-basics/src/index.tsx
@@ -2,15 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
-import {store} from './store/store';
-
+import { store } from './store/store';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
