3a545cc1cab27290afea76eca6233a69f0a8bb08
SofiaKubo <skuboreva@gmail.com>
2026-03-13 15:16:53 +0100

    feat: fetch tracks on app mount

--- sptint_11/12-fetching-tracks-with-create-async-thunk/src/app/index.tsx
+++ sptint_11/12-fetching-tracks-with-create-async-thunk/src/app/index.tsx
@@ -1,17 +1,23 @@
-import {Layout} from '../components/Layout/Layout';
-import {TrackList} from '../components/TrackList/TrackList';
-import {useSelector} from 'react-redux';
-import {selectIsLoading, selectTracks} from '../slices/tracksSlice';
-import {RootState} from '../store/store';
-import {TrackModel} from '../models/Track';
+import { useEffect } from 'react';
+import { Layout } from '../components/Layout/Layout';
+import { TrackList } from '../components/TrackList/TrackList';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTracks, selectIsLoading, selectTracks } from '../slices/tracksSlice';
+import { AppDispatch, RootState } from '../store/store';
+import { TrackModel } from '../models/Track';
 
 export const App = () => {
-	const tracks = useSelector<RootState, TrackModel[]>(selectTracks);
-	const isLoading = useSelector<RootState, boolean>((selectIsLoading));
+  const dispatch = useDispatch<AppDispatch>();
+  const tracks = useSelector<RootState, TrackModel[]>(selectTracks);
+  const isLoading = useSelector<RootState, boolean>(selectIsLoading);
 
-	return (
-		<Layout>
-			<TrackList tracks={tracks} isLoading={isLoading}/>
-		</Layout>
-	)
-}
+  useEffect(() => {
+    dispatch(fetchTracks());
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <TrackList tracks={tracks} isLoading={isLoading} />
+    </Layout>
+  );
+};
