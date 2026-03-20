0cc60955107f8de46210c17fcd3b2a8eb36ceacf
SofiaKubo <skuboreva@gmail.com>
2026-03-20 11:38:08 +0100

    fix: remove header from app routes

--- sptint_11/14-react-router-navigation/src/app/index.tsx
+++ sptint_11/14-react-router-navigation/src/app/index.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Header } from '../components/Header';
 import { Home } from '../pages/Home';
 import { HotelList } from '../pages/HotelList';
 import { SignIn } from '../pages/SignIn';
@@ -8,7 +7,6 @@ import { SignIn } from '../pages/SignIn';
 export const App = () => {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotel-list" element={<HotelList />} />
